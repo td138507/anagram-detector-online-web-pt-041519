765203ce671c321af56f88923ebb353b35a0c1a9
Taylor Doyle <tcd56@cornell.edu>
2019-06-23 17:21:21 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+   def initialize(words)
+    @word = words
+  end
+
+   def match(word)
+    word.select {|word| word.split("").sort == @word.split("").sort}
+  end
+
+ end
